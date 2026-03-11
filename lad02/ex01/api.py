5ade25d1a434162afdf67f1dfd7a3b7befa15455
npquoc_1855 <1111111wda@gmail.com>
2026-03-11 14:27:31 +0700

    [add] lab-02 railfence

--- lad02/ex01/api.py
+++ lad02/ex01/api.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from cipher.vigenere import VigenereCipher
+from cipher.railfence import RailFenceCipher
 
 app = Flask(__name__)
 
 vigenere_cipher = VigenereCipher()
+railfence_cipher = RailFenceCipher()
 
 @app.route('/api/vigenere/encrypt', methods=['POST'])
 def vigenere_encrypt():
@@ -19,6 +21,22 @@ def vigenere_decrypt():
     cipher_text = data['cipher_text']
     key = data['key']
     decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+
+@app.route('/api/railfence/encrypt', methods=['POST'])
+def rail_fence_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    num_rails = int(data['num_rails'])
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, num_rails)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/railfence/decrypt', methods=['POST'])
+def rail_fence_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    num_rails = int(data['num_rails'])
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, num_rails)
     return jsonify({'decrypted_text': decrypted_text})
 
 if __name__ == '__main__':
